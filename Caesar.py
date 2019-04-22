75291483fd661f24ec1edf6a88bf6ebccd2191c4
UARTman <agus179e@gmail.com>
2019-04-22 15:04:11 +0300

    Caesar Code: Added functional encoder/decoder

--- Caesar.py
+++ Caesar.py
@@ -2,7 +2,17 @@ CDICT = 'абвгдеёжзийклмнопрстуфхцчщьъыьэюя'
 HCDICT = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 LDICT = 'abcdefghijklmnopqrstuvwxyz'
 HLDICT = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+DICTS = [CDICT, HCDICT, LDICT, HLDICT]
 
-
-if __name__ == '__main__':
-    pass
+def caesar(str0='', dicts=DICTS, step=1):
+    str1=''
+    for i in str0:
+        k = False
+        for j in dicts:
+            if i in j:
+                str1+=(j[(j.index(i) + step) % len(j)])
+                k = True
+                break
+        if not k:
+            str1 += i
+    return str1
